b572cacbc735d4c02619c7a563e9fa4d8f0e74cc
Jonathan Candelaria <candelariajr@appstate.edu>
2022-06-21 08:33:34 -0400

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,11 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta content="no-cache, no-store, must-revalidate" http-equiv="cache-control"/>
+    <meta http-equiv="pragma" content="no-cache">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Down For Maintenance</title>
+    <title>Site Down For Maintenance</title>
     <style>
 
     </style>
