1b6dda0f0b4ffbcfd38cd5cbd83fd3d606f4b474
Jonathan Candelaria <candelariajr@appstate.edu>
2022-06-21 08:39:11 -0400

    Update index.php
    Don't allow maintenance pages to be indexed.

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta http-equiv="pragma" content="no-cache">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="robots" content="noindex, nofollow">
     <title>Site Down For Maintenance</title>
     <style>
 
